c4c9f6f41364499bd92545fc4399988ed7cf8f28
GiovanniAdelfio <147213043+GiovanniAdelfio@users.noreply.github.com>
2025-09-01 18:41:07 +0200

    Implement Procustes weight-matching in main function
    We are calculating the proc_dict containing the orthogonal matrices for every Layer directly in the repository.

--- matching/mlp_mnist_weight_matching.py
+++ matching/mlp_mnist_weight_matching.py
@@ -9,6 +9,7 @@ from utils.training import test
 from tqdm import tqdm
 import copy
 import matplotlib.pyplot as plt
+from utils.weight_matching import Procustes, proc_weight_matching_MLP
 
 
 def main():
@@ -16,7 +17,6 @@ def main():
     parser.add_argument("--model_a", type=str, required=True)
     parser.add_argument("--model_b", type=str, required=True)
     parser.add_argument("--seed", type=int, default=0, help="Random seed")
-    parser.add_argument("--proc_dict", type = dict, required = False)
     args = parser.parse_args()
 
     # load models
@@ -84,30 +84,29 @@ def main():
       train_acc_interp_clever.append(acc)
 
 
-    # procustes weight-matching if provided
-    if hasattr(args, "proc_dict"):
-        proc_dict = args.prog_dict
-        model_a.load_state_dict(checkpoint)
-        model_b.load_state_dict(checkpoint_b)
-        
-        model_a_dict = copy.deepcopy(model_a.state_dict())
-        model_weights = [['layer0.weight', 'layer0.bias'], ['layer1.weight', 'layer1.bias'], ['layer2.weight', 'layer2.bias'], ['layer3.weight', 'layer3.bias'], ['layer4.weight', 'layer4.bias']]
-        for num, layer in enumarate(['layer0.weight', 'layer1.weight', 'layer2.weight', 'layer3.weight']):
-            Q = proc_dict[layer]
-            for key in model_weights[num]:
-                model_a_dict[key] = (model_a_dict[key].T @ Q).T
-            if num <3 :
-                for key in model_weights[num+1]:
-                    model_a_dict[key] = (Q.T @ model_a_dict[key].T).T
+    # procustes weight-matching 
+    model_a.load_state_dict(checkpoint)
+    model_b.load_state_dict(checkpoint_b)
+    proc_dict = proc_weight_matching_MLP(checkpoint, checkpoint_b)
+    
+    model_a_dict = copy.deepcopy(model_a.state_dict())
+    model_weights = [['layer0.weight', 'layer0.bias'], ['layer1.weight', 'layer1.bias'], ['layer2.weight', 'layer2.bias'], ['layer3.weight', 'layer3.bias'], ['layer4.weight', 'layer4.bias']]
+    for num, layer in enumarate(['layer0.weight', 'layer1.weight', 'layer2.weight', 'layer3.weight']):
+        Q = proc_dict[layer]
+        for key in model_weights[num]:
+            model_a_dict[key] = (model_a_dict[key].T @ Q).T
+        if num <3 :
+            for key in model_weights[num+1]:
+                model_a_dict[key] = (Q.T @ model_a_dict[key].T).T
 
-        model_b_dict = copy.deepcopy(model_b.state_dict())
-        for lam in tqdm(lambdas):
-          naive_p = lerp(lam, model_a_dict, model_b_dict)
-          model_b.load_state_dict(naive_p)
-          test_loss, acc = test(model_b.cuda(), 'cuda', test_loader)
-          test_acc_interp_proc.append(acc)
-          train_loss, acc = test(model_b.cuda(), 'cuda', train_loader)
-          train_acc_interp_proc.append(acc)
+    model_b_dict = copy.deepcopy(model_b.state_dict())
+    for lam in tqdm(lambdas):
+      naive_p = lerp(lam, model_a_dict, model_b_dict)
+      model_b.load_state_dict(naive_p)
+      test_loss, acc = test(model_b.cuda(), 'cuda', test_loader)
+      test_acc_interp_proc.append(acc)
+      train_loss, acc = test(model_b.cuda(), 'cuda', train_loader)
+      train_acc_interp_proc.append(acc)
             
     fig = plot_interp_acc(lambdas, train_acc_interp_naive, test_acc_interp_naive,
                     train_acc_interp_clever, test_acc_interp_clever, train_acc_interp_proc, test_acc_interp_proc)

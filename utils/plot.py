a37cc1ff1975c4d3ab58863a453512a2b82112d8
GiovanniAdelfio <147213043+GiovanniAdelfio@users.noreply.github.com>
2025-09-01 18:27:10 +0200

    Add test and train accuracy plots for procustes

--- utils/plot.py
+++ utils/plot.py
@@ -23,11 +23,63 @@ def plot_interp_acc(lambdas, train_acc_interp_naive, test_acc_interp_naive,
           linestyle="solid",
           color="tab:blue",
           linewidth=2,
+          label="Train, interp. via Procustes")
+  ax.plot(lambdas,
+          test_acc_interp_clever,
+          linestyle="solid",
+          color="tab:orange",
+          linewidth=2,
+          label="Test, interp. via Procustes")
+  ax.set_xlabel("$\lambda$")
+  ax.set_xticks([0, 1])
+  ax.set_xticklabels(["Model $A$", "Model $B$"])
+  ax.set_ylabel("Accuracy")
+  # TODO label x=0 tick as \theta_1, and x=1 tick as \theta_2
+  ax.set_title(f"Accuracy between the two models")
+  ax.legend(loc="lower right", framealpha=0.5)
+  fig.tight_layout()
+  return fig
+
+def plot_interp_acc_2(lambdas, train_acc_interp_naive, test_acc_interp_naive,
+                    train_acc_interp_clever, test_acc_interp_clever, train_acc_interp_proc, test_acc_interp_proc):
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  ax.plot(lambdas,
+          train_acc_interp_naive,
+          linestyle="dashed",
+          color="tab:blue",
+          alpha=0.5,
+          linewidth=2,
+          label="Train, naïve interp.")
+  ax.plot(lambdas,
+          test_acc_interp_naive,
+          linestyle="dashed",
+          color="tab:orange",
+          alpha=0.5,
+          linewidth=2,
+          label="Test, naïve interp.")
+  ax.plot(lambdas,
+          train_acc_interp_clever,
+          linestyle="solid",
+          color="tab:blue",
+          linewidth=2,
           label="Train, permuted interp.")
   ax.plot(lambdas,
           test_acc_interp_clever,
           linestyle="solid",
           color="tab:orange",
+          linewidth=2,
+          label="Test, permuted interp.")
+  ax.plot(lambdas,
+          train_acc_interp_proc,
+          linestyle="solid",
+          color="tab:yellow",
+          linewidth=2,
+          label="Test, permuted interp.")
+  ax.plot(lambdas,
+          test_acc_interp_proc,
+          linestyle="solid",
+          color="tab:green",
           linewidth=2,
           label="Test, permuted interp.")
   ax.set_xlabel("$\lambda$")

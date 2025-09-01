2a8845a82ec10cba60d7700145cc5ead32eb89ef
GiovanniAdelfio <147213043+GiovanniAdelfio@users.noreply.github.com>
2025-09-01 18:45:23 +0200

    Just re-ordering the code

--- matching/mlp_mnist_weight_matching.py
+++ matching/mlp_mnist_weight_matching.py
@@ -57,6 +57,34 @@ def main():
     train_acc_interp_clever = []
     train_acc_interp_naive = []
     train_acc_interp_proc = []
+
+
+
+
+    # procustes weight-matching
+    proc_dict = proc_weight_matching_MLP(checkpoint, checkpoint_b)
+    
+    model_a_dict = copy.deepcopy(model_a.state_dict())
+    model_weights = [['layer0.weight', 'layer0.bias'], ['layer1.weight', 'layer1.bias'], ['layer2.weight', 'layer2.bias'], ['layer3.weight', 'layer3.bias'], ['layer4.weight', 'layer4.bias']]
+    for num, layer in enumarate(['layer0.weight', 'layer1.weight', 'layer2.weight', 'layer3.weight']):
+        Q = proc_dict[layer]
+        for key in model_weights[num]:
+            model_a_dict[key] = (model_a_dict[key].T @ Q).T
+        if num <3 :
+            for key in model_weights[num+1]:
+                model_a_dict[key] = (Q.T @ model_a_dict[key].T).T
+
+    model_b_dict = copy.deepcopy(model_b.state_dict())
+    for lam in tqdm(lambdas):
+      naive_p = lerp(lam, model_a_dict, model_b_dict)
+      model_b.load_state_dict(naive_p)
+      test_loss, acc = test(model_b.cuda(), 'cuda', test_loader)
+      test_acc_interp_proc.append(acc)
+      train_loss, acc = test(model_b.cuda(), 'cuda', train_loader)
+      train_acc_interp_proc.append(acc)
+
+
+    
     # naive
     model_b.load_state_dict(checkpoint_b)
     model_a_dict = copy.deepcopy(model_a.state_dict())
@@ -83,30 +111,6 @@ def main():
       train_loss, acc = test(model_b.cuda(), 'cuda', train_loader)
       train_acc_interp_clever.append(acc)
 
-
-    # procustes weight-matching 
-    model_a.load_state_dict(checkpoint)
-    model_b.load_state_dict(checkpoint_b)
-    proc_dict = proc_weight_matching_MLP(checkpoint, checkpoint_b)
-    
-    model_a_dict = copy.deepcopy(model_a.state_dict())
-    model_weights = [['layer0.weight', 'layer0.bias'], ['layer1.weight', 'layer1.bias'], ['layer2.weight', 'layer2.bias'], ['layer3.weight', 'layer3.bias'], ['layer4.weight', 'layer4.bias']]
-    for num, layer in enumarate(['layer0.weight', 'layer1.weight', 'layer2.weight', 'layer3.weight']):
-        Q = proc_dict[layer]
-        for key in model_weights[num]:
-            model_a_dict[key] = (model_a_dict[key].T @ Q).T
-        if num <3 :
-            for key in model_weights[num+1]:
-                model_a_dict[key] = (Q.T @ model_a_dict[key].T).T
-
-    model_b_dict = copy.deepcopy(model_b.state_dict())
-    for lam in tqdm(lambdas):
-      naive_p = lerp(lam, model_a_dict, model_b_dict)
-      model_b.load_state_dict(naive_p)
-      test_loss, acc = test(model_b.cuda(), 'cuda', test_loader)
-      test_acc_interp_proc.append(acc)
-      train_loss, acc = test(model_b.cuda(), 'cuda', train_loader)
-      train_acc_interp_proc.append(acc)
             
     fig = plot_interp_acc(lambdas, train_acc_interp_naive, test_acc_interp_naive,
                     train_acc_interp_clever, test_acc_interp_clever, train_acc_interp_proc, test_acc_interp_proc)

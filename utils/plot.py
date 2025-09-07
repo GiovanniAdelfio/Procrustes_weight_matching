e5cecb952c32e4dc373c7fa1f5f5a9fd11fe4bdf
GiovanniAdelfio <147213043+GiovanniAdelfio@users.noreply.github.com>
2025-09-07 10:49:57 +0200

    Comment functions

--- utils/plot.py
+++ utils/plot.py
@@ -2,8 +2,14 @@ import matplotlib.pyplot as plt
 
 def plot_interp_acc(lambdas, train_acc_interp_naive, test_acc_interp_naive,
                     train_acc_interp_clever, test_acc_interp_clever, train_acc_interp_proc, test_acc_interp_proc):
+
+""" 
+Plots 3 different interpolation train accuracies and 3 test accuracies on the same plot
+"""
+
   fig = plt.figure()
   ax = fig.add_subplot(111)
+
   ax.plot(lambdas,
           train_acc_interp_naive,
           linestyle="dashed",
@@ -42,17 +48,23 @@ def plot_interp_acc(lambdas, train_acc_interp_naive, test_acc_interp_naive,
           color="tab:orange",
           linewidth=2,
           label="Test, weight matching via Procrustes")
+
   ax.set_xlabel("$\lambda$")
   ax.set_xticks([0, 1])
   ax.set_xticklabels(["Model $A$", "Model $B$"])
   ax.set_ylabel("Accuracy")
   # TODO label x=0 tick as \theta_1, and x=1 tick as \theta_2
+
   ax.set_title(f"Accuracy between the two models")
   ax.legend(loc="lower right", framealpha=0.5)
   fig.tight_layout()
   return fig
 
+
 def plot_interp_acc_2(lambdas, train_acc_interp_naive, test_acc_interp_naive, train_acc_interp_proc, test_acc_interp_proc):
+"""
+Same as the previous one, just with only 2 train and test accuracy.
+"""
   fig = plt.figure()
   ax = fig.add_subplot(111)
   ax.plot(lambdas,
